08aeca6d7a341a3bb5279d7d32f8eb79ef150750
aralotibi <aralotibi@quinnipiac.edu>
2015-11-06 06:38:20 +0000

    Add school list view

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ urlpatterns = patterns('',
     url(r'^user/', include('registration.backends.simple.urls')),
     url(r'^user/', include('django.contrib.auth.urls')),
     url(r'^school/create/$', SchoolCreateView.as_view(), name='school_create'),
+    url(r'school/$', SchoolListView.as_view(), name='school_list'),
 )
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.views.generic import CreateView
+from django.views.generic import ListView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
 
@@ -12,8 +13,12 @@ class SchoolCreateView(CreateView):
     model = School
     template_name = "school/school_form.html"
     fields = ['title', 'description']
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('school_list')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(SchoolCreateView, self).form_valid(form)
+
+class SchoolListView(ListView):
+    model = School
+    template_name = "school/school_list.html"
