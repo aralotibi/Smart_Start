4725e21697693a520ed46f2b56b33f54a9f5864d
aralotibi <aralotibi@quinnipiac.edu>
2015-11-06 05:38:27 +0000

    Add school create view

--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic import CreateView
+from django.core.urlresolvers import reverse_lazy
+from .models import *
 
 # Create your views here.
 class Home(TemplateView):
     template_name = "home.html"
+
+class SchoolCreateView(CreateView):
+    model = School
+    template_name = "school/school_form.html"
+    fields = ['title', 'description']
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(SchoolCreateView, self).form_valid(form)
