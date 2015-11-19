af7285e3b25afda708ee81faddcca14752012e9a
aralotibi <aralotibi@quinnipiac.edu>
2015-11-19 20:29:15 +0000

    Add some fix

--- core/views.py
+++ core/views.py
@@ -146,9 +146,10 @@ class UserDetailView(DetailView):
     slug_field = 'username'
     template_name = 'user/user_detail.html'
     context_object_name = 'user_in_view'
+
     def get_context_data(self, **kwargs):
       context = super(UserDetailView, self).get_context_data(**kwargs)
-      user_in_view = User.objects.get(username=self.kwargs['slug'])
+      user_in_view = User.objects.get(username = self.kwargs['slug'])
       schools = School.objects.filter(user=user_in_view).exclude(visibility=1)
       context['schools'] = schools
       reviews = Review.objects.filter(user=user_in_view).exclude(visibility=1)
